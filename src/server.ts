0aae87026cb612e94edfba443649ade1041a539a
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2019-05-18 09:32:54 -0500

    Add DNSProxy class.

--- src/server.ts
+++ src/server.ts
@@ -22,13 +22,6 @@ const logger = winston.createLogger({
     transports: [new winston.transports.Console()]
 });
 
-const getRandomDNSID = (): number => {
-    const getRandomInt = (min: number, max: number): number => {
-        return Math.floor(Math.random() * (max - min + 1)) + min
-    };
-    return getRandomInt(1, 65534);
-}
-
 const SERVER_PORT = 10053;
 
 const REMOTE_IP = '8.8.8.8';
@@ -95,203 +88,218 @@ const getNowSeconds = (): number => {
     return now.getTime() / 1000.;
 };
 
-const originalTTLSymbol = Symbol('originalTTL');
+class DNSProxy {
 
-const getMinTTLSecondsForAnswers = (answers: any): number | undefined => {
-    let minTTL: number | undefined;
-    (answers || []).forEach((answer: any) => {
-        if ((answer.ttl === undefined) || (answer.ttl === null) || (answer.ttl < MIN_TTL_SECONDS)) {
-            answer.ttl = MIN_TTL_SECONDS;
-        }
-        answer[originalTTLSymbol] = answer.ttl;
-        if ((minTTL === undefined) || (answer.ttl < minTTL)) {
-            minTTL = answer.ttl;
-        }
-    });
-    return minTTL;
-};
-
-const adjustTTL = (cacheObject: CacheObject): boolean => {
-    let valid = true;
-
-    const nowSeconds = getNowSeconds();
-
-    const secondsUntilExpiration = cacheObject.expirationTimeSeconds - nowSeconds;
-
-    if (secondsUntilExpiration <= 0) {
-        valid = false;
-    } else {
-        const secondsInCache = nowSeconds - cacheObject.cacheTimeSeconds;
-        (cacheObject.decodedResponse.answers || []).forEach((answer: any) => {
-            answer.ttl = answer[originalTTLSymbol] - secondsInCache;
-            // logger.info(`new answer.ttl = ${answer.ttl}`);
-        });
+    private getRandomDNSID(): number {
+        const getRandomInt = (min: number, max: number): number => {
+            return Math.floor(Math.random() * (max - min + 1)) + min
+        };
+        return getRandomInt(1, 65534);
     }
 
-    return valid;
-};
+    private static originalTTLSymbol = Symbol('originalTTL');
 
-const main = () => {
-    logger.info('begin main');
+    private getMinTTLSecondsForAnswers(answers: any): number | undefined {
+        let minTTL: number | undefined;
+        (answers || []).forEach((answer: any) => {
+            if ((answer.ttl === undefined) || (answer.ttl === null) || (answer.ttl < MIN_TTL_SECONDS)) {
+                answer.ttl = MIN_TTL_SECONDS;
+            }
+            answer[DNSProxy.originalTTLSymbol] = answer.ttl;
+            if ((minTTL === undefined) || (answer.ttl < minTTL)) {
+                minTTL = answer.ttl;
+            }
+        });
+        return minTTL;
+    }
 
-    const outgoingIDToRequestInfo = new Map<number, OutgoingRequestInfo>();
-    const outgoingRequestInfoPriorityQueue = new TinyQueue<OutgoingRequestInfo>([], (a: OutgoingRequestInfo, b: OutgoingRequestInfo) => a.compareByExpirationTime(b));
-
-    const questionToResponse = new Map<string, CacheObject>();
-    const questionToResponsePriorityQueue = new TinyQueue<CacheObject>([], (a: CacheObject, b: CacheObject) => a.compareByExpirationTime(b));
-
-    let cacheHits = 0;
-    let cacheMisses = 0;
-
-    setInterval(() => {
-        logger.info('begin timer pop');
+    private adjustTTL(cacheObject: CacheObject): boolean {
+        let valid = true;
 
         const nowSeconds = getNowSeconds();
-        let done: boolean;
 
-        done = false;
-        const expiredOutgoingIDs: number[] = [];
-        while ((outgoingRequestInfoPriorityQueue.length > 0) && (!done)) {
-            const outgoingRequestInfo = outgoingRequestInfoPriorityQueue.peek();
-            if (outgoingRequestInfo && outgoingRequestInfo.expired(nowSeconds)) {
-                outgoingRequestInfoPriorityQueue.pop();
-                expiredOutgoingIDs.push(outgoingRequestInfo.outgoingRequestID);
-            } else {
-                done = true;
-            }
+        const secondsUntilExpiration = cacheObject.expirationTimeSeconds - nowSeconds;
+
+        if (secondsUntilExpiration <= 0) {
+            valid = false;
+        } else {
+            const secondsInCache = nowSeconds - cacheObject.cacheTimeSeconds;
+            (cacheObject.decodedResponse.answers || []).forEach((answer: any) => {
+                answer.ttl = answer[DNSProxy.originalTTLSymbol] - secondsInCache;
+                // logger.info(`new answer.ttl = ${answer.ttl}`);
+            });
         }
-        expiredOutgoingIDs.forEach((id) => {
-            outgoingIDToRequestInfo.delete(id);
+
+        return valid;
+    }
+
+    start() {
+        logger.info('begin start');
+
+        const outgoingIDToRequestInfo = new Map<number, OutgoingRequestInfo>();
+        const outgoingRequestInfoPriorityQueue = new TinyQueue<OutgoingRequestInfo>([], (a: OutgoingRequestInfo, b: OutgoingRequestInfo) => a.compareByExpirationTime(b));
+
+        const questionToResponse = new Map<string, CacheObject>();
+        const questionToResponsePriorityQueue = new TinyQueue<CacheObject>([], (a: CacheObject, b: CacheObject) => a.compareByExpirationTime(b));
+
+        let cacheHits = 0;
+        let cacheMisses = 0;
+
+        setInterval(() => {
+            logger.info('begin timer pop');
+
+            const nowSeconds = getNowSeconds();
+            let done: boolean;
+
+            done = false;
+            const expiredOutgoingIDs: number[] = [];
+            while ((outgoingRequestInfoPriorityQueue.length > 0) && (!done)) {
+                const outgoingRequestInfo = outgoingRequestInfoPriorityQueue.peek();
+                if (outgoingRequestInfo && outgoingRequestInfo.expired(nowSeconds)) {
+                    outgoingRequestInfoPriorityQueue.pop();
+                    expiredOutgoingIDs.push(outgoingRequestInfo.outgoingRequestID);
+                } else {
+                    done = true;
+                }
+            }
+            expiredOutgoingIDs.forEach((id) => {
+                outgoingIDToRequestInfo.delete(id);
+            });
+
+            done = false;
+            const expiredQuestionCacheKeys: string[] = [];
+            while ((questionToResponsePriorityQueue.length > 0) && (!done)) {
+                const cacheObject = questionToResponsePriorityQueue.peek();
+                if (cacheObject && cacheObject.expired(nowSeconds)) {
+                    questionToResponsePriorityQueue.pop();
+                    expiredQuestionCacheKeys.push(cacheObject.questionCacheKey);
+                } else {
+                    done = true;
+                }
+            }
+            expiredQuestionCacheKeys.forEach((questionCacheKey) => {
+                questionToResponse.delete(questionCacheKey);
+            });
+
+            logger.info(`end timer pop cacheHits=${cacheHits} cacheMisses=${cacheMisses}` +
+                ` expiredOutgoingIDs=${expiredOutgoingIDs.length} outgoingIDToRequestInfo=${outgoingIDToRequestInfo.size} outgoingRequestInfoPriorityQueue=${outgoingRequestInfoPriorityQueue.length}` +
+                ` expiredQuestionCacheKeys=${expiredQuestionCacheKeys.length} questionToResponse=${questionToResponse.size} questionToResponsePriorityQueue=${questionToResponsePriorityQueue.length}`);
+
+        }, TIMER_INTERVAL_SECONDS * 1000);
+
+        const serverSocket = dgram.createSocket('udp4');
+        const remoteSocket = dgram.createSocket('udp4');
+
+        serverSocket.on('error', (err) => {
+            logger.warn(`serverSocketError ${formatError(err)}`);
+            process.exit(1);
         });
 
-        done = false;
-        const expiredQuestionCacheKeys: string[] = [];
-        while ((questionToResponsePriorityQueue.length > 0) && (!done)) {
-            const cacheObject = questionToResponsePriorityQueue.peek();
-            if (cacheObject && cacheObject.expired(nowSeconds)) {
-                questionToResponsePriorityQueue.pop();
-                expiredQuestionCacheKeys.push(cacheObject.questionCacheKey);
-            } else {
-                done = true;
-            }
-        }
-        expiredQuestionCacheKeys.forEach((questionCacheKey) => {
-            questionToResponse.delete(questionCacheKey);
+        serverSocket.on('listening', () => {
+            logger.info(`serverSocket listening on ${stringify(serverSocket.address())}`);
         });
 
-        logger.info(`end timer pop cacheHits=${cacheHits} cacheMisses=${cacheMisses}` +
-            ` expiredOutgoingIDs=${expiredOutgoingIDs.length} outgoingIDToRequestInfo=${outgoingIDToRequestInfo.size} outgoingRequestInfoPriorityQueue=${outgoingRequestInfoPriorityQueue.length}` +
-            ` expiredQuestionCacheKeys=${expiredQuestionCacheKeys.length} questionToResponse=${questionToResponse.size} questionToResponsePriorityQueue=${questionToResponsePriorityQueue.length}`);
+        serverSocket.on('message', (message: Buffer, remoteInfo: dgram.RemoteInfo) => {
+            const decodedObject = dnsPacket.decode(message, null);
 
-    }, TIMER_INTERVAL_SECONDS * 1000);
+            let cacheHit = false;
 
-    const serverSocket = dgram.createSocket('udp4');
-    const remoteSocket = dgram.createSocket('udp4');
+            if (decodedObject.questions &&
+                (decodedObject.questions.length === 1)) {
 
-    serverSocket.on('error', (err) => {
-        logger.warn(`serverSocketError ${formatError(err)}`);
-        process.exit(1);
-    });
-
-    serverSocket.on('listening', () => {
-        logger.info(`serverSocket listening on ${stringify(serverSocket.address())}`);
-    });
-
-    serverSocket.on('message', (message: Buffer, remoteInfo: dgram.RemoteInfo) => {
-        const decodedObject = dnsPacket.decode(message, null);
-
-        let cacheHit = false;
-
-        if (decodedObject.questions &&
-            (decodedObject.questions.length === 1)) {
-
-            const question = decodedObject.questions[0];
-            const questionCacheKey = `${question.name}_${question.type}_${question.class}`;
-
-            const cacheObject = questionToResponse.get(questionCacheKey);
-            if (cacheObject && adjustTTL(cacheObject)) {
-                const cachedResponse = cacheObject.decodedResponse;
-                cachedResponse.id = decodedObject.id;
-
-                const outgoingMessage = dnsPacket.encode(cachedResponse, null, null);
-                serverSocket.send(outgoingMessage, 0, outgoingMessage.length, remoteInfo.port, remoteInfo.address);
-
-                cacheHit = true;
-
-                ++cacheHits;
-            }
-        }
-
-        if (!cacheHit) {
-            ++cacheMisses;
-
-            const outgoingID = getRandomDNSID();
-
-            const requestTimeoutSeconds = getNowSeconds() + REQUEST_TIMEOUT_SECONDS;
-
-            const outgoingRequestInfo =
-                new OutgoingRequestInfo(outgoingID, remoteInfo, decodedObject.id, requestTimeoutSeconds);
-
-            outgoingRequestInfoPriorityQueue.push(outgoingRequestInfo);
-            outgoingIDToRequestInfo.set(outgoingID, outgoingRequestInfo);
-
-            decodedObject.id = outgoingID;
-            const outgoingMessage = dnsPacket.encode(decodedObject, null, null);
-
-            remoteSocket.send(outgoingMessage, 0, outgoingMessage.length, REMOTE_PORT, REMOTE_IP);
-        }
-    });
-
-    remoteSocket.on('error', (err) => {
-        logger.warn(`remoteSocket ${formatError(err)}`);
-        process.exit(1);
-    });
-
-    remoteSocket.on('listening', () => {
-        logger.info(`remoteSocket listening on ${stringify(remoteSocket.address())}`);
-
-        serverSocket.bind(SERVER_PORT);
-    });
-
-    remoteSocket.on('message', (message: Buffer, remoteInfo: dgram.RemoteInfo) => {
-        const decodedObject = dnsPacket.decode(message, null);
-        // logger.info(`remoteSocket message remoteInfo = ${stringifyPretty(remoteInfo)}\ndecodedObject = ${stringifyPretty(decodedObject)}`);
-
-        if ((decodedObject.rcode === 'NOERROR') &&
-            decodedObject.questions &&
-            (decodedObject.questions.length === 1)) {
-
-            const minTTLSeconds = getMinTTLSecondsForAnswers(decodedObject.answers);
-
-            if ((minTTLSeconds !== undefined) && (minTTLSeconds > 0)) {
                 const question = decodedObject.questions[0];
                 const questionCacheKey = `${question.name}_${question.type}_${question.class}`;
 
-                const nowSeconds = getNowSeconds();
-                const expirationTimeSeconds = nowSeconds + minTTLSeconds;
+                const cacheObject = questionToResponse.get(questionCacheKey);
+                if (cacheObject && this.adjustTTL(cacheObject)) {
+                    const cachedResponse = cacheObject.decodedResponse;
+                    cachedResponse.id = decodedObject.id;
 
-                const cacheObject = new CacheObject(
-                    questionCacheKey, decodedObject, nowSeconds, expirationTimeSeconds);
+                    const outgoingMessage = dnsPacket.encode(cachedResponse, null, null);
+                    serverSocket.send(outgoingMessage, 0, outgoingMessage.length, remoteInfo.port, remoteInfo.address);
 
-                questionToResponsePriorityQueue.push(cacheObject);
-                questionToResponse.set(questionCacheKey, cacheObject);
+                    cacheHit = true;
+
+                    ++cacheHits;
+                }
             }
-        }
 
-        const clientRequestInfo = outgoingIDToRequestInfo.get(decodedObject.id);
-        if (clientRequestInfo) {
-            outgoingIDToRequestInfo.delete(decodedObject.id);
+            if (!cacheHit) {
+                ++cacheMisses;
 
-            decodedObject.id = clientRequestInfo.clientRequestID;
-            const outgoingMessage = dnsPacket.encode(decodedObject, null, null);
+                const outgoingID = this.getRandomDNSID();
 
-            const clientRemoteInfo = clientRequestInfo.clientRemoteInfo;
+                const requestTimeoutSeconds = getNowSeconds() + REQUEST_TIMEOUT_SECONDS;
 
-            serverSocket.send(outgoingMessage, 0, outgoingMessage.length, clientRemoteInfo.port, clientRemoteInfo.address);
-        }
-    });
+                const outgoingRequestInfo =
+                    new OutgoingRequestInfo(outgoingID, remoteInfo, decodedObject.id, requestTimeoutSeconds);
 
-    remoteSocket.bind();
+                outgoingRequestInfoPriorityQueue.push(outgoingRequestInfo);
+                outgoingIDToRequestInfo.set(outgoingID, outgoingRequestInfo);
+
+                decodedObject.id = outgoingID;
+                const outgoingMessage = dnsPacket.encode(decodedObject, null, null);
+
+                remoteSocket.send(outgoingMessage, 0, outgoingMessage.length, REMOTE_PORT, REMOTE_IP);
+            }
+        });
+
+        remoteSocket.on('error', (err) => {
+            logger.warn(`remoteSocket ${formatError(err)}`);
+            process.exit(1);
+        });
+
+        remoteSocket.on('listening', () => {
+            logger.info(`remoteSocket listening on ${stringify(remoteSocket.address())}`);
+
+            serverSocket.bind(SERVER_PORT);
+        });
+
+        remoteSocket.on('message', (message: Buffer, remoteInfo: dgram.RemoteInfo) => {
+            const decodedObject = dnsPacket.decode(message, null);
+            // logger.info(`remoteSocket message remoteInfo = ${stringifyPretty(remoteInfo)}\ndecodedObject = ${stringifyPretty(decodedObject)}`);
+
+            if ((decodedObject.rcode === 'NOERROR') &&
+                decodedObject.questions &&
+                (decodedObject.questions.length === 1)) {
+
+                const minTTLSeconds = this.getMinTTLSecondsForAnswers(decodedObject.answers);
+
+                if ((minTTLSeconds !== undefined) && (minTTLSeconds > 0)) {
+                    const question = decodedObject.questions[0];
+                    const questionCacheKey = `${question.name}_${question.type}_${question.class}`;
+
+                    const nowSeconds = getNowSeconds();
+                    const expirationTimeSeconds = nowSeconds + minTTLSeconds;
+
+                    const cacheObject = new CacheObject(
+                        questionCacheKey, decodedObject, nowSeconds, expirationTimeSeconds);
+
+                    questionToResponsePriorityQueue.push(cacheObject);
+                    questionToResponse.set(questionCacheKey, cacheObject);
+                }
+            }
+
+            const clientRequestInfo = outgoingIDToRequestInfo.get(decodedObject.id);
+            if (clientRequestInfo) {
+                outgoingIDToRequestInfo.delete(decodedObject.id);
+
+                decodedObject.id = clientRequestInfo.clientRequestID;
+                const outgoingMessage = dnsPacket.encode(decodedObject, null, null);
+
+                const clientRemoteInfo = clientRequestInfo.clientRemoteInfo;
+
+                serverSocket.send(outgoingMessage, 0, outgoingMessage.length, clientRemoteInfo.port, clientRemoteInfo.address);
+            }
+        });
+
+        remoteSocket.bind();
+    }
+
+};
+
+const main = () => {
+    new DNSProxy().start();
 };
 
 main();
