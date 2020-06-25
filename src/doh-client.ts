23fed3b85b599e7f1add382c0add165edbcb7645
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2020-06-25 18:51:21 -0500

    Add doh-json module.

--- src/doh-client.ts
+++ src/doh-client.ts
@@ -1,50 +1,9 @@
 import * as configuration from './configuration.js';
+import * as dohJson from './doh-json.js';
 import { logger } from './logging.js';
 import * as utils from './utils.js';
-import dnsPacket, { answer } from 'dns-packet';
+import dnsPacket from 'dns-packet';
 import http2 from 'http2';
-import { stringify } from 'querystring';
-
-interface DOHJSONResponseAnswer {
-    name?: string;
-    type?: number;
-    TTL?: number;
-    data?: string;
-}
-
-interface DOHJSONResponse {
-    Status?: number;
-    Answer?: DOHJSONResponseAnswer[];
-};
-
-const decodeDOHJSONRRType = (type?: number): string | undefined => {
-    if (!utils.isNumber(type)) {
-        logger.warn(`got non-number RR type in DOH response: ${type}`);
-        return undefined;
-    }
-
-    switch (type) {
-        case 1:
-            return 'A';
-        case 5:
-            return 'CNAME';
-        case 6:
-            return 'SOA';
-        case 12:
-            return 'PTR';
-        case 16:
-            return 'TXT';
-        case 28:
-            return 'AAAA';
-        case 33:
-            return 'SRV';
-        case 43:
-            return 'DS';
-    }
-
-    logger.warn(`got unknown RR type in DOH response: ${type}`);
-    return undefined;
-};
 
 // https://tools.ietf.org/html/rfc8484
 export class Http2RemoteServerConnection {
@@ -137,7 +96,7 @@ export class Http2RemoteServerConnection {
                     reject(new Error('responseChunks empty'));
                 } else {
                     const responseBuffer = Buffer.concat(responseChunks);
-                    const response = this.decodeJSONResponse(dnsRequest, responseBuffer.toString());
+                    const response = dohJson.decodeJSONResponse(dnsRequest, responseBuffer.toString());
                     if (!response) {
                         request.close();
                         reject(new Error('error decoding dns packet'));
@@ -150,63 +109,6 @@ export class Http2RemoteServerConnection {
             request.end();
 
         });
-    }
-
-    private decodeJSONResponse(dnsRequest: dnsPacket.DNSPacket, responseString: string): dnsPacket.DNSPacket | undefined {
-        try {
-            const responseObject = JSON.parse(responseString) as DOHJSONResponse;
-
-            const dnsResponsePacket = dnsRequest;
-
-            dnsResponsePacket.type = 'response';
-
-            if (dnsResponsePacket.flags === undefined) {
-                dnsResponsePacket.flags = 0;
-            }
-
-            // RA
-            dnsResponsePacket.flags |= (1 << 7);
-            // RD
-            dnsResponsePacket.flags |= (1 << 8);
-
-            if (responseObject.Status === 0) {
-                dnsResponsePacket.rcode = 'NOERROR';
-            } else if (responseObject.Status === 2) {
-                dnsResponsePacket.rcode = 'SERVFAIL';
-                dnsResponsePacket.flags |= 2;
-            } else if (responseObject.Status === 3) {
-                dnsResponsePacket.rcode = 'NXDOMAIN';
-                dnsResponsePacket.flags |= 3;
-            } else {
-                logger.warn(`got unknown responseObject.Status=${responseObject.Status}`);
-                dnsResponsePacket.rcode = 'SERVFAIL';
-                dnsResponsePacket.flags |= 2;
-            }
-
-            dnsResponsePacket.answers =
-                (responseObject.Answer || []).map(answer => {
-                    // strip leading and trailing .
-                    const name = answer.name?.replace(/^\.|\.$/gm, '');
-                    const data = answer.data?.replace(/^\.|\.$/gm, '');
-
-                    return {
-                        name: name,
-                        type: decodeDOHJSONRRType(answer.type),
-                        ttl: answer.TTL,
-                        class: 'IN',
-                        data: data
-                    };
-                });
-
-            dnsResponsePacket.authorities = [];
-            dnsResponsePacket.additionals = [];
-
-            return dnsResponsePacket;
-        } catch (err) {
-            logger.error(`decodeJSONResponse error err = ${utils.formatError(err)}`);
-        }
-
-        return undefined;
     }
 
     private createSessionIfNecessary() {
---
+++ src/doh-json.ts
@@ -0,0 +1,118 @@
+import dnsPacket from 'dns-packet';
+import { logger } from './logging.js';
+import * as utils from './utils.js';
+
+interface DOHJSONResponseQuestion {
+    name?: string;
+    type?: number;
+}
+
+interface DOHJSONResponseAnswer {
+    name?: string;
+    type?: number;
+    TTL?: number;
+    data?: string;
+}
+
+interface DOHJSONResponse {
+    Status?: number;
+    Question?: DOHJSONResponseQuestion[];
+    Answer?: DOHJSONResponseAnswer[];
+};
+
+const decodeDOHJSONRRType = (type?: number): string | undefined => {
+    if (!utils.isNumber(type)) {
+        logger.warn(`got non-number RR type in DOH response: ${type}`);
+        return undefined;
+    }
+
+    switch (type) {
+        case 1:
+            return 'A';
+        case 5:
+            return 'CNAME';
+        case 6:
+            return 'SOA';
+        case 12:
+            return 'PTR';
+        case 16:
+            return 'TXT';
+        case 28:
+            return 'AAAA';
+        case 33:
+            return 'SRV';
+        case 43:
+            return 'DS';
+    }
+
+    logger.warn(`got unknown RR type in DOH response: ${type}`);
+    return undefined;
+};
+
+export const decodeJSONResponse = (dnsRequest: dnsPacket.DNSPacket, responseString: string): dnsPacket.DNSPacket | undefined => {
+    try {
+        const responseObject = JSON.parse(responseString) as DOHJSONResponse;
+
+        const dnsResponsePacket: dnsPacket.DNSPacket = {
+            id: dnsRequest.id,
+            type: 'response'
+        };
+
+        dnsResponsePacket.flags = 0;
+
+        // RA
+        dnsResponsePacket.flags |= (1 << 7);
+        // RD
+        dnsResponsePacket.flags |= (1 << 8);
+
+        if (responseObject.Status === 0) {
+            dnsResponsePacket.rcode = 'NOERROR';
+        } else if (responseObject.Status === 2) {
+            dnsResponsePacket.rcode = 'SERVFAIL';
+            dnsResponsePacket.flags |= 2;
+        } else if (responseObject.Status === 3) {
+            dnsResponsePacket.rcode = 'NXDOMAIN';
+            dnsResponsePacket.flags |= 3;
+        } else {
+            logger.warn(`got unknown responseObject.Status=${responseObject.Status}`);
+            dnsResponsePacket.rcode = 'SERVFAIL';
+            dnsResponsePacket.flags |= 2;
+        }
+
+        dnsResponsePacket.questions =
+            (responseObject.Question ?? []).map(question => {
+                // strip leading and trailing .
+                const name = question.name?.replace(/^\.|\.$/gm, '');
+
+                return {
+                    name: name,
+                    type: decodeDOHJSONRRType(question.type),
+                    class: 'IN',
+                };
+            });
+
+        dnsResponsePacket.answers =
+            (responseObject.Answer ?? []).map(answer => {
+                // strip leading and trailing .
+                const name = answer.name?.replace(/^\.|\.$/gm, '');
+                const data = answer.data?.replace(/^\.|\.$/gm, '');
+
+                return {
+                    name: name,
+                    type: decodeDOHJSONRRType(answer.type),
+                    ttl: answer.TTL,
+                    class: 'IN',
+                    data: data
+                };
+            });
+
+        dnsResponsePacket.authorities = [];
+        dnsResponsePacket.additionals = [];
+
+        return dnsResponsePacket;
+    } catch (err) {
+        logger.error(`decodeJSONResponse error err = ${utils.formatError(err)}`);
+    }
+
+    return undefined;
+}
